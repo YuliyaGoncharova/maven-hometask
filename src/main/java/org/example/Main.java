dce92ccd34da9520b377693a8a30681ec55fb3f1
ulka <ulka@ulka.com>
2023-06-13 15:11:54 +0200

    2nd commit of maven-hometask

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,27 +5,9 @@ import org.example.dto.PersonDto;
 
 public class Main {
     public static void main(String[] args) {
-//        String personData = "{\n"
-//            + "    \"name\": \"Alya\",\n"
-//            +"    \"lastName\": \"Zablotska\"\n"
-//            +"}";
-//        PersonDto person = new Gson().fromJson(personData, PersonDto.class);
-//        System.out.println(person.getName() + " " + person.getLastName());
 
-        PersonDto person = new PersonDto("Yulia", "Zablotska");
-
-//        Gson personToGson = new Gson();
-//        String personToJson = personToGson.toJson(person);
-//        String personToJson = new Gson().toJson(person);
-//        System.out.println(personToJson);
-        System.out.println("My data in Json = " + new Gson().toJson(person));
-
-
-
-
-
-
-
+        PersonDto person = new PersonDto("Yuliia", "Zablotska");
+        System.out.println(new Gson().toJson(person));
 
     }
 }
